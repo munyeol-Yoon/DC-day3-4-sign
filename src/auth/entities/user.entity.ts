c1fdcd92bab40460e87774510b9c4bdc2d68d65c
muyeon <gmakin36@gmail.com>
2024-03-06 15:31:49 +0900

    feat: add validation through class-validator
    class-validator 를 사용해 UserModel 에 검증 추가
    PickType 을 활용하기 위해 모델에 class-validator 추가

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,26 +1,35 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { BaseModel } from 'src/common/entities/base.entity';
 import { Column, Entity } from 'typeorm';
 
 @Entity()
 export class UserModel extends BaseModel {
+  @IsString()
+  @IsNotEmpty()
   @Column({
     type: 'varchar',
     length: 30,
   })
   name: string;
 
+  @IsEmail()
+  @IsNotEmpty()
   @Column({
     type: 'varchar',
     length: 30,
   })
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
   @Column({
     type: 'varchar',
     length: 50,
   })
   phone: string;
 
+  @IsString()
+  @IsNotEmpty()
   @Column({
     type: 'varchar',
     length: 60,
